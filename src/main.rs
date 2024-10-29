a9cf4ec437e6ae6c72afeeed4c7e050d0ec91194
Chris Davison <c.jr.davison@gmail.com>
2024-10-29 13:12:49 +0000

    add excludes to an

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,8 @@ enum Command {
         /// Show in descending, not ascending order
         #[clap(short, long)]
         reverse: bool,
+        #[clap(short, long)]
+        exclude: Vec<String>,
     },
     /// How many headers
     Headercount {
@@ -40,6 +42,8 @@ enum Command {
         /// Show in descending, not ascending order
         #[clap(short, long)]
         reverse: bool,
+        #[clap(short, long)]
+        exclude: Vec<String>,
     },
     /// Filesize in bytes
     #[clap(alias = "bytes")]
@@ -52,6 +56,8 @@ enum Command {
         /// Show in descending, not ascending order
         #[clap(short, long)]
         reverse: bool,
+        #[clap(short, long)]
+        exclude: Vec<String>,
     },
     /// Wordcount
     #[clap(aliases = &["w", "wc"])]
@@ -64,6 +70,8 @@ enum Command {
         /// Show in descending, not ascending order
         #[clap(short, long)]
         reverse: bool,
+        #[clap(short, long)]
+        exclude: Vec<String>,
     },
     /// Reading time (estimate)
     Time {
@@ -75,11 +83,15 @@ enum Command {
         /// Show in descending, not ascending order
         #[clap(short, long)]
         reverse: bool,
+        #[clap(short, long)]
+        exclude: Vec<String>,
     },
     /// ToC of each file
     Toc {
         /// Which files to operate on, or all under cwd
         files: Vec<String>,
+        #[clap(short, long)]
+        exclude: Vec<String>,
     },
     /// Show broken links
     Links {
@@ -88,6 +100,8 @@ enum Command {
         /// Only run on local links
         #[clap(short = 'l', long = "local")]
         local: bool,
+        #[clap(short, long)]
+        exclude: Vec<String>,
     },
     /// Show tags for each file
     Tags {
@@ -99,17 +113,23 @@ enum Command {
         /// Tags that the file must NOT have
         #[clap(short = 'n')]
         not: Vec<String>,
+        #[clap(short, long)]
+        exclude: Vec<String>,
     },
     /// Show untagged files
     Untagged {
         /// Which files to operate on, or all under cwd
         files: Vec<String>,
+        #[clap(short, long)]
+        exclude: Vec<String>,
     },
     /// Search notes, returning matching filenames
     Search {
         /// Words to search for
         #[clap(required = true)]
         query: Vec<String>,
+        #[clap(short, long)]
+        exclude: Vec<String>,
     },
 }
 
@@ -117,39 +137,72 @@ fn main() -> Result<()> {
     let opts = Cli::parse();
 
     match opts.command {
-        Command::Complexity { files, n, reverse } => {
-            analyse::note_complexity(&files_or_curdir(&files)?, n, reverse)
+        Command::Complexity {
+            files,
+            n,
+            reverse,
+            exclude,
+        } => analyse::note_complexity(&files_or_curdir(&files, &exclude)?, n, reverse),
+        Command::Headercount {
+            files,
+            n,
+            reverse,
+            exclude,
+        } => analyse::note_header_count(&files_or_curdir(&files, &exclude)?, n, reverse),
+        Command::Size {
+            files,
+            n,
+            reverse,
+            exclude,
+        } => analyse::note_size(&files_or_curdir(&files, &exclude)?, n, reverse),
+        Command::Words {
+            files,
+            n,
+            reverse,
+            exclude,
+        } => {
+            let files_filtered: Vec<_> = files_or_curdir(&files, &exclude)?
+                .iter()
+                .filter(|x| !exclude.iter().any(|e| x.contains(e)))
+                .cloned()
+                .collect();
+            analyse::wordcount(&files_filtered, n, reverse)
         }
-        Command::Headercount { files, n, reverse } => {
-            analyse::note_header_count(&files_or_curdir(&files)?, n, reverse)
-        }
-        Command::Size { files, n, reverse } => {
-            analyse::note_size(&files_or_curdir(&files)?, n, reverse)
-        }
-        Command::Words { files, n, reverse } => {
-            analyse::wordcount(&files_or_curdir(&files)?, n, reverse)
-        }
-        Command::Time { files, n, reverse } => {
-            analyse::reading_time(&files_or_curdir(&files)?, n, reverse)
-        }
+        Command::Time {
+            files,
+            n,
+            reverse,
+            exclude,
+        } => analyse::reading_time(&files_or_curdir(&files, &exclude)?, n, reverse),
 
-        Command::Toc { files } => analyse::note_structure(&files_or_curdir(&files)?),
-        Command::Links { files, local } => links::broken_links(&files_or_curdir(&files)?, local),
-        Command::Untagged { files } => tags::display_untagged_files(&files_or_curdir(&files)?),
-        Command::Search { query } => search::search(&files_or_curdir(&[])?, &query),
+        Command::Toc { files, exclude } => {
+            analyse::note_structure(&files_or_curdir(&files, &exclude)?)
+        }
+        Command::Links {
+            files,
+            local,
+            exclude,
+        } => links::broken_links(&files_or_curdir(&files, &exclude)?, local),
+        Command::Untagged { files, exclude } => {
+            tags::display_untagged_files(&files_or_curdir(&files, &exclude)?)
+        }
+        Command::Search { query, exclude } => {
+            search::search(&files_or_curdir(&[], &exclude)?, &query)
+        }
         Command::Tags {
             files,
             keywords,
             not,
+            exclude,
         } => {
             let filter = Filter::new(keywords.as_slice(), not.as_slice(), false);
-            tags::display_tags_for_each(filter, &files_or_curdir(&files)?)
+            tags::display_tags_for_each(filter, &files_or_curdir(&files, &exclude)?)
         }
     }
 }
 
 /// If files is empty, return md files under the currend directory
-fn files_or_curdir(files: &[String]) -> Result<Vec<String>> {
+fn files_or_curdir(files: &[String], exclude: &[String]) -> Result<Vec<String>> {
     let mut out = Vec::new();
     let mut files = files.to_vec();
     if files.is_empty() {
@@ -164,5 +217,10 @@ fn files_or_curdir(files: &[String]) -> Result<Vec<String>> {
             out.push(thing.to_string());
         }
     }
+    out = out
+        .iter()
+        .filter(|f| !exclude.iter().any(|e| f.contains(e)))
+        .cloned()
+        .collect::<Vec<_>>();
     Ok(out)
 }
